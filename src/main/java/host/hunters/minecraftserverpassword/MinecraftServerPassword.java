5992a1bc90ea3ff7bf272b2e30fb3eff65c97f22
Hunter Webb <oh@users.noreply.github.com>
2022-08-25 20:03:32 -0400

    Initial commit

---
+++ src/main/java/host/hunters/minecraftserverpassword/MinecraftServerPassword.java
@@ -0,0 +1,23 @@
+// Written by github.com/oh & github.com/insyri
+//
+// MinecraftServerPassword allows users to set a password for their server to disallow unknown users from moving,
+// breaking blocks, or chatting. Once a user has entered the password once, they will be added to a whitelist and will
+// not have to enter the password again upon joining the server.
+
+package host.hunters.minecraftserverpassword;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MinecraftServerPassword extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
