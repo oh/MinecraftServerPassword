bcfbcf6c621ec0112f2720770204c5f053e2cabe
Hunter Webb <oh@users.noreply.github.com>
2022-09-04 21:44:40 -0400

    Changes by insyri

--- src/main/kotlin/host/hunters/minecraftserverpassword/OnMessage.kt
+++ src/main/kotlin/host/hunters/minecraftserverpassword/OnMessage.kt
@@ -23,8 +23,6 @@ class OnMessage(private val plugin : MinecraftServerPassword) : Listener {
     @EventHandler
     fun onMessage(event : AsyncChatEvent) {
 
-        val p = event.player
-
         if (whitelist.contains(event.player.uniqueId.toString())) return
 
         for (p : Player in Bukkit.getOnlinePlayers()) {
@@ -33,45 +31,37 @@ class OnMessage(private val plugin : MinecraftServerPassword) : Listener {
 
         if (event.message().asComponent() == Component.text(plugin.config.get("password").toString())) {
 
-            whitelist.add(p.uniqueId.toString())
+            whitelist.add(event.player.uniqueId.toString())
             plugin.config.set("whitelistedUsers", whitelist)
             plugin.saveConfig()
 
-            if (p.hasPermission("serverpassword.whitelisted")) {
+            if (event.player.hasPermission("serverpassword.whitelisted")) {
                 println("working")
                 plugin.server.scheduler.runTaskLater(plugin, Runnable {
                     run {
-                        p.removePotionEffect(PotionEffectType.BLINDNESS)
-                        p.gameMode = GameMode.SURVIVAL
-                        p.walkSpeed = 0.2F
+                        event.player.removePotionEffect(PotionEffectType.BLINDNESS)
+                        event.player.gameMode = GameMode.SURVIVAL
+                        event.player.walkSpeed = 0.2F
                     }
                 }, 1L)
-            } else {
-                println("not working")
-            }
+            } else println("not working")
 
-            p.sendMessage("You have been added to the whitelist.")
-
-            return
+            event.player.sendMessage("You have been added to the whitelist.")
         }
 
         attempts--
 
         if (attempts >= 0) {
-            blacklist.add(p.uniqueId.toString())
+            blacklist.add(event.player.uniqueId.toString())
             plugin.config.set("blacklistedUsers", blacklist)
             plugin.saveConfig()
             plugin.server.scheduler.runTaskLater(plugin, Runnable {
                 run {
-                    p.kick(Component.text("You have incorrectly attempted the password too many times and have been blacklisted."))
+                    event.player.kick(Component.text("You have incorrectly attempted the password too many times and have been blacklisted."))
                 }
             }, 1L)
-
-            return
         }
 
-        p.sendMessage("The password you entered is incorrect. You have $attempts attempts left.")
-
-        return
+        event.player.sendMessage("The password you entered is incorrect. You have $attempts attempts left.")
     }
 }
